97f07ddf43dbe0548691fb573656ce28f6bd537a
Benoit Gallois <63228475+bgallois255@users.noreply.github.com>
2023-05-20 14:59:57 +0200

    Update __init__.py

--- src/openpifpaf/plugins/animalpose/__init__.py
+++ src/openpifpaf/plugins/animalpose/__init__.py
@@ -1,13 +1,8 @@
 
 import openpifpaf
 
-from . import animal_kp
-
-from .animal_kp import AnimalKp
+from . import traffic_sign_kp
 
 
 def register():
-    openpifpaf.DATAMODULES['animal'] = animal_kp.AnimalKp
-    openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-animalpose'] = \
-        "http://github.com/vita-epfl/openpifpaf-torchhub/releases/" \
-        "download/v0.12.9/shufflenetv2k30-210511-120906-animal.pkl.epoch400"
+    openpifpaf.DATAMODULES['traffic_sign'] = traffic_sign_kp.TrafficSignKp
